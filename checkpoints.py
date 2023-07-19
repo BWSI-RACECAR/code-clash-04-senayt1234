dfd7dfb1566b367382f0d868b48a95a9307e6206
Senay <senaytesfamicael@gmail.com>
2023-07-19 11:46:25 -0400

    4

--- checkpoints.py
+++ checkpoints.py
@@ -12,15 +12,15 @@ Difficulty Level: 4/10
 
 Prompt: The Grand Prix is an obstacle course for the RACECAR. Each of the RACECARs must traverse the course 
 and navigate through checkpoints. At the beginning of each obstacle, there will be a checkpoint, at which the 
-RACECAR can stop to refuel. Before the course starts, each competitor is given an array of values that indicate 
+RACECAR can stop to refuel. Before the course starts, each competitor is given an checkpointsay of values that indicate 
 the distance of these checkpoints (in meters) relative to the starting line. In order to prepare your RACECAR 
 for the final race, you want to calculate the longest distance between two checkpoints such that your car has 
 enough fuel to last the entire leg.
 
-Given an array of checkpoint distance values, return an integer to represent the longest distance between two checkpoints.
+Given an checkpointsay of checkpoint distance values, return an integer to represent the longest distance between two checkpoints.
 
-Constraints: All numbers in the array “n” will be integers 10^9 >= n >= 0. Assume the number of items in the 
-array “k” will be 10^9 >= k > 0. You may not use the Python sort() method.
+Constraints: All numbers in the checkpointsay “n” will be integers 10^9 >= n >= 0. Assume the number of items in the 
+checkpointsay “k” will be 10^9 >= k > 0. You may not use the Python sort() method.
 
 Test Cases:
 Input: [0, 3, 4, 9] ; Output: 5
@@ -36,12 +36,12 @@ class Solution:
             
             #TODO: Write code below to returnn an int with the solution to the prompt.
             index = 0
-            for i in range(0, len(arr)):    
-                for j in range(i+1, len(arr)):    
-                    if(arr[i] > arr[j]):    
-                        index = arr[i]    
-                        arr[i] = arr[j]    
-                        arr[j] = index
+            for i in range(0, len(checkpoints)):    
+                for j in range(i+1, len(checkpoints)):    
+                    if(checkpoints[i] > checkpoints[j]):    
+                        index = checkpoints[i]    
+                        checkpoints[i] = checkpoints[j]    
+                        checkpoints[j] = index
             index = 0
             big = 0
             while len(checkpoints)>index+1:
@@ -52,12 +52,12 @@ class Solution:
             return big
 
 def main():
-    array = input().split(" ")
-    for x in range (0, len(array)):
-        array[x] = int(array[x])
+    checkpointsay = input().split(" ")
+    for x in range (0, len(checkpointsay)):
+        checkpointsay[x] = int(checkpointsay[x])
 
     tc1 = Solution()
-    ans = tc1.longestdistance(array)
+    ans = tc1.longestdistance(checkpointsay)
     print(ans)
 
 if __name__ and "__main__":
